756a0c8842c1cc7138e1c5ea3e21a95d3ebff1d7
leonkiong <70616398+leonkiong@users.noreply.github.com>
2025-08-20 19:24:00 +0800

    Update server.js

--- server.js
+++ server.js
@@ -1,6 +1,11 @@
-const express = require("express");
+const express = require('express');
 const app = express();
-const port = process.env.PORT || 5678;
+const port = process.env.PORT || 10000;
 
-app.get("/", (req, res) => res.send("Worker running 🚀"));
-app.listen(port, () => console.log(`Health check server on port ${port}`));
+app.get('/', (req, res) => {
+  res.send('Dummy server to keep Render alive.');
+});
+
+app.listen(port, () => {
+  console.log(`Dummy server running on port ${port}`);
+});
